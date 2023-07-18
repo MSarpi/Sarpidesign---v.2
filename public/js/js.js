5b42cc1a103b2ed7314e3b8772f1b26784af61ee
MSarpi <miguel.sarpi@gmail.com>
2023-07-17 23:06:36 -0400

    egrego cambios para estados de OT

--- public/js/js.js
+++ public/js/js.js
@@ -132,3 +132,65 @@ function toggleDarkMode() {
         img2.classList.add('transition-img');
     }
 }
+
+function formulario(){
+	var screen = $('#loading-screen');
+    configureLoadingScreen(screen);
+
+	var gmail = document.getElementById('email').value;
+	var nom = document.getElementById('name').value;
+	var text = document.getElementById('textarea').value;
+	var val = 0;
+	var error = "";
+
+	if (gmail == "") {
+		val = 1;
+		error += '<p style="margin: 0 0 0 5rem"><i style="color:red; margin-right: 10px;" class="fas fa-times"></i>Ingrese su <span style="color: #ff9065;">Correo</span></p>';
+	}
+
+	if (nom == "") {
+		val = 1;
+		error += '<p style="margin: 0 0 0 5rem"><i style="color:red; margin-right: 10px;" class="fas fa-times"></i>Ingrese su <span style="color: #ff9065;">Nombre</span> o <span style="color: #ff9065;">Empresa</span></p>';
+	}
+
+	if (text == "") {
+		val = 1;
+		error += '<p style="margin: 0 0 0 5rem"><i style="color:red; margin-right: 10px;" class="fas fa-times"></i>Ingrese un <span style="color: #ff9065;">Mensaje</span></p>';
+	}
+
+	if (val == 0) {
+		var gmail = document.getElementById('email').value;
+		var nom = document.getElementById('name').value;
+		var text = document.getElementById('textarea').value;
+
+		$.ajax({
+			type: "POST",
+			url: "https://formsubmit.io/send/670d39ef-8b75-43db-9fe1-e39e7a75f9d4",
+			dataType: 'json',
+			accepts: 'application/json',
+			data: {
+				Nombre: nom,
+				Gmail: gmail,
+				Informacion: text
+			},
+			success: function (res) {
+				
+			},
+			error:function (erro) {
+                alert("no sube");
+			}
+		});
+	}else{
+		alert("faltan datos");
+	}
+}
+
+function configureLoadingScreen(screen){
+    $(document)
+        .ajaxStart(function () {
+            screen.fadeIn();
+        })
+        .ajaxStop(function () {
+            screen.fadeOut();
+        });
+}
