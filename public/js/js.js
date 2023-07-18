c9f2058f975914665c4b4c0385791d67ca0620af
MSarpi <miguel.sarpi@gmail.com>
2023-07-18 15:26:21 -0400

    cambios finales pagina web

--- public/js/js.js
+++ public/js/js.js
@@ -1,4 +1,21 @@
-/*INICIO-EFECTO DE ESCRITURA*/
+const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+// Mostrar el botón cuando el usuario haya bajado 20% de la altura de la página
+window.addEventListener("scroll", () => {
+  if (window.scrollY > window.innerHeight * 0.2) {
+    scrollToTopBtn.style.opacity = "1";
+  } else {
+    scrollToTopBtn.style.opacity = "0";
+  }
+});
+
+// Volver al principio de la página cuando se haga clic en el botón
+scrollToTopBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth" // Suavizar el scroll para que sea más agradable
+  });
+});
 
 const modal = document.getElementById('modal');
 const body = document.body;
